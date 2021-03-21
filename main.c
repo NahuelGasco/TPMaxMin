e155e2572c74cc99c1736bca48e64c2557464a47
NahuelGasco <NahuMurphys@outlook.com>
2021-03-20 21:04:32 -0300

    Resolucion del TPMAXMIN

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
 
+    int nota, suma, promedio,i, notasTotales;
+    nota=0;
+    suma=0;
+    promedio=0;
+    notasTotales=0;
+
+    printf("Ingrese cantidad de notas totales \n");
+    scanf("%d", &notasTotales);
+
+    for (i=0; i<notasTotales ; i++)
+    {
+
+        printf("Ingrese nota \n");
+        scanf("%d", &nota);
+
+        suma=suma+nota;
+    }
+    promedio=suma/notasTotales;
+    printf("La suma total de notas es: %d \n", suma);
+    printf("El promedio es; %d", promedio);
+
     return 0;
+
 }
