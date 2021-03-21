efb4572bba7e3fee4121a9c09c179ec6576cd59d
NahuelGasco <NahuMurphys@outlook.com>
2021-03-21 02:18:37 -0300

    Modificacion de TPmaxminif

--- main.c
+++ main.c
@@ -2,26 +2,35 @@
 #include <stdlib.h>
 
 
-int main()
-{
+int main(){
 
-    int nota, suma, promedio,i, notasTotales;
+    int nota, suma, promedio,i, notasTotales, minimo, maximo;
     nota=0;
     suma=0;
     promedio=0;
     notasTotales=0;
+    maximo=0;
+    minimo=10;
 
     printf("Ingrese cantidad de notas totales \n");
     scanf("%d", &notasTotales);
 
-    for (i=0; i<notasTotales ; i++)
-    {
+    for (i=0; i<notasTotales ; i++) {
 
         printf("Ingrese nota \n");
         scanf("%d", &nota);
 
         suma=suma+nota;
+        if(nota>maximo){
+            maximo=nota;
+        }
+        if (nota<minimo){
+            minimo=nota;
+        }
     }
+
+    printf("La nota max es: %d \n", maximo);
+    printf("La nota Min es: %d \n", minimo);
     promedio=suma/notasTotales;
     printf("La suma total de notas es: %d \n", suma);
     printf("El promedio es; %d", promedio);
