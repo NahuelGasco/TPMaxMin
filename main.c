bcff51d3d1fd110bfde012761f16bea48eaa4c9a
NahuelGasco <NahuMurphys@outlook.com>
2021-03-22 01:02:08 -0300

    Terminacion de maxmin exitosa

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 
-int main(){
+int main()
+{
 
     int nota, suma, promedio,i, notasTotales, minimo, maximo;
     nota=0;
@@ -10,30 +11,40 @@ int main(){
     promedio=0;
     notasTotales=0;
     maximo=0;
-    minimo=10;
+    minimo=0;
 
     printf("Ingrese cantidad de notas totales \n");
     scanf("%d", &notasTotales);
 
-    for (i=0; i<notasTotales ; i++) {
+    for (i=0; i<notasTotales ; i++)
+    {
 
         printf("Ingrese nota \n");
         scanf("%d", &nota);
 
         suma=suma+nota;
-        if(nota>maximo){
+        if(i==0){
             maximo=nota;
         }
-        if (nota<minimo){
+        else{
+            if(nota>maximo){
+                maximo=nota;
+            }
+        }
+        if(i==0){
             minimo=nota;
         }
+        else{
+            if(nota<minimo){
+                minimo=nota;
+            }
+        }
     }
-
+    promedio=suma/notasTotales;
     printf("La nota max es: %d \n", maximo);
     printf("La nota Min es: %d \n", minimo);
-    promedio=suma/notasTotales;
     printf("La suma total de notas es: %d \n", suma);
-    printf("El promedio es; %d", promedio);
+    printf("El promedio es; %d \n", promedio);
 
     return 0;
 
